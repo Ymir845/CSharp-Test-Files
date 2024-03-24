4c296d7674bf6672bb82fc1d30686217909b85b1
Ymir <160019304+Ymir845@users.noreply.github.com>
2024-03-25 09:57:27 +1100

    Update and rename Hello World to Hello World.cs

---
+++ Console/Hello World.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+
+class ConsoleTest{
+
+    static void Main(string[]args)
+     {
+
+        int Variable1 = 2;
+        int Variable2 = 2;
+
+        System.Console.WriteLine("Hello World");
+
+        System.Console.WriteLine(Variable1);
+        
+        System.Console.WriteLine(Variable2 + Variable1);
+
+    }
+}
