58c8af28181dcc147429037ce6c4df1ede4438a6
Ymir <160019304+Ymir845@users.noreply.github.com>
2024-03-25 22:00:21 +0000

    Synt and Exec

---
+++ Console/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(2+2);
+
+int Ethan = 12;
+
+if (Ethan == 12)
+{  
+    Console.WriteLine("nigger");
+}
+
+string Google = "good";
+
+if (Google != "bad")
+
+{
+    Console.WriteLine("real");
+}
