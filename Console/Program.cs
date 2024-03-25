223b0522ebc96fb427cbcfd7b37a4a5632064bfa
Ymir <160019304+Ymir845@users.noreply.github.com>
2024-03-25 22:02:06 +0000

    IF ELSE

---
+++ Console/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program{
+    static void Main(string[]args) {
+
+        string Entrance = "Welcome! What is your name";
+
+        System.Console.WriteLine(Entrance);
+
+        string playerName = Console.ReadLine();
+
+        System.Console.WriteLine("Are you sure your name is " + playerName);
+
+        string playerAnswer = Console.ReadLine();
+
+        if (playerAnswer == "yes") {
+
+            System.Console.WriteLine("Okay! Nice to meet you " + playerName);
+
+        }
+        else {
+            System.Console.WriteLine("Oh");
+        }
+
+    }
+}
